fae286d7b0d719679e7ae803582ae04031b56a22
KonviaNuva <100172888+KonviaNuva@users.noreply.github.com>
2026-01-13 23:02:48 +0300

    more gameplay UI

--- src/main/java/org/example/GameplayScreen.java
+++ src/main/java/org/example/GameplayScreen.java
@@ -8,10 +8,14 @@ public class GameplayScreen extends JPanel {
     private static GameplayScreen gameplayScreen;
 
     private JPanel backButtonPanel;
+    private JPanel instructionPanel;
+    private JPanel attemptsPanel;
 
     private JButton backButton;
+    private JLabel instructionLabel;
 
     private final Font buttonFont = new Font("Times New Roman", Font.PLAIN, 40);
+    private final Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
 
     public static GameplayScreen getGameplayScreen() {
         if (gameplayScreen == null) {
@@ -28,9 +32,21 @@ public class GameplayScreen extends JPanel {
 
         backButtonPanel = new JPanel();
         backButtonPanel.setBounds(0, 0, 140, 70);
-        backButtonPanel.setBackground(Color.BLACK);
+        backButtonPanel.setBackground(Color.YELLOW);
         backButtonPanel.setVisible(true);
         this.add(backButtonPanel);
+
+        instructionPanel = new JPanel();
+        instructionPanel.setBounds(100, 100, 200, 50);
+        instructionPanel.setBackground(Color.BLUE);
+        instructionPanel.setVisible(true);
+        this.add(instructionPanel);
+
+        attemptsPanel = new JPanel();
+        attemptsPanel.setBounds(400, 0, 400, 600);
+        attemptsPanel.setBackground(Color.CYAN);
+        attemptsPanel.setVisible(true);
+        this.add(attemptsPanel);
 
         backButton = new JButton("Back");
         backButton.setBackground(Color.BLACK);
@@ -38,5 +54,10 @@ public class GameplayScreen extends JPanel {
         backButton.setFont(buttonFont);
         backButton.setFocusPainted(false);
         backButtonPanel.add(backButton);
+
+        instructionLabel = new JLabel("Enter the code");
+        instructionLabel.setForeground(Color.WHITE);
+        instructionLabel.setFont(normalFont);
+        instructionPanel.add(instructionLabel);
     }
 }
